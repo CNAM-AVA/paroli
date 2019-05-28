52498f3fa660f20c9de213fe9c1b95e2a8c7c46f
Nautilus Jungle <iamnautilusjungle@gmail.com>
2019-05-28 10:48:43 +0200

    done

--- routes.js
+++ routes.js
@@ -2,3 +2,4 @@ const routes = require('next-routes')
                                                     // Name   Page   Pattern
 module.exports = routes()                           // ----   ----   -----
 .add('sub', '/p/:slug', 'sub')                      // sub    sub    /p/:slug
+.add('post', '/p/:slug/:post', 'post')              // post   post   /p/:slug/:post
