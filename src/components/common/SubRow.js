adb6c2c31a38556ba31713b59761485bd2b89d2f
antoine plard <azertyzhd@gmail.com>
2019-06-13 23:20:32 +0200

    style (subs): loading animation on subscribe and unsbscribe buttons

--- src/components/common/SubRow.js
+++ src/components/common/SubRow.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid, Avatar, Typography, Button, Link } from '@material-ui/core'
+import { Grid, Avatar, Typography, Button, Link, CircularProgress } from '@material-ui/core'
 import firebase from '../../../lib/firebase'
 import { subscribeToSub, isUserSubbed, unsubscribeToSub } from '../../../lib/sub'
 import Sub from '../../../database/models/Sub';
+import yellow from '@material-ui/core/colors/yellow';
 
 const styles = theme => ({
     communities: {
         marginTop: 10,
         marginBottom: 10
-    }
+    },
+    buttonProgress: {
+        color: yellow[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+    },
+    wrapper: {
+        margin: theme.spacing.unit,
+        position: 'relative',
+    },
 })
 
 class SubRow extends React.Component {
@@ -19,18 +32,19 @@ class SubRow extends React.Component {
         super(props);
         this.state = {
             uid: null,
-            subButtonIsOnSubscribe: false
+            subButtonIsOnSubscribe: false,
+            loadingbtn: false
         }
     }
 
     async componentWillMount() {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-                this.setState({uid: user.uid})
+                this.setState({ uid: user.uid })
                 this.updateSubsButton();
             }
             else
-                this.setState({uid: null})
+                this.setState({ uid: null })
         })
     }
 
@@ -60,6 +74,10 @@ class SubRow extends React.Component {
 
     async subscribe() {
 
+        this.setState({
+            loadingbtn: true
+        })
+
         let sub = await Sub.getByName(this.props.community.name);
         const subName = sub.docs[0].data().name;
 
@@ -68,35 +86,50 @@ class SubRow extends React.Component {
         subscribeToSub(this.state.uid, subName).then(() => {
             console.log('subbed');
             this.setState({
-                subButtonIsOnSubscribe: true
+                subButtonIsOnSubscribe: true,
+                loadingbtn: false
             })
         }).catch((e) => {
-            console.log(e); 
+            console.log(e);
         })
     }
 
     async unsubscribe() {
+
+        this.setState({
+            loadingbtn: true
+        })
 
         let sub = await Sub.getByName(this.props.community.name);
         const subName = sub.docs[0].data().name;
 
         unsubscribeToSub(this.state.uid, subName).then(() => {
             this.setState({
-                subButtonIsOnSubscribe: false
+                subButtonIsOnSubscribe: false,
+                loadingbtn: false
             })
         }).catch((e) => {
-            console.log(e); 
+            console.log(e);
         })
     }
 
     subButtonHandler() {
+
+        const { classes } = this.props;
+
         if (this.state.subButtonIsOnSubscribe) {
-            return(
-                <Button variant={"contained"} color={"secondary"} onClick={() => this.unsubscribe()}>Désabonner</Button>
+            return (
+                <div className={classes.wrapper}>
+                    <Button variant={"contained"} color={"secondary"} onClick={() => this.unsubscribe()}>Désabonner</Button>
+                    {this.state.loadingbtn && <CircularProgress size={24} className={classes.buttonProgress} />}
+                </div>
             )
         } else {
-            return(
-                <Button variant={"contained"} color={"primary"} onClick={() => this.subscribe()}>Rejoindre</Button>
+            return (
+                <div className={classes.wrapper}>
+                    <Button variant={"contained"} color={"primary"} onClick={() => this.subscribe()}>Rejoindre</Button>
+                    {this.state.loadingbtn && <CircularProgress size={24} className={classes.buttonProgress} />}
+                </div>
             )
         }
     }
@@ -114,7 +147,7 @@ class SubRow extends React.Component {
                             <Avatar className={classes.avatar}>{community.name.charAt(0).toUpperCase()}</Avatar>
                         </Grid>
                         <Grid item xs={9}>
-                            <Typography><Link href={"/p/"+community.name}>p/{community.name}</Link></Typography>
+                            <Typography><Link href={"/p/" + community.name}>p/{community.name}</Link></Typography>
                             <Typography>{community.subscribersCount} subscribers</Typography>
                         </Grid>
                     </Grid>
